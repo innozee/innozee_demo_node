f88b0d857de2a94714d0a9e8140bd8bd2146cd2e
InnoZee <115397260+innozee@users.noreply.github.com>
2024-01-04 10:10:23 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// npm init -y
+// npm install fastify
+// npm i sqlite3
+// npm i websocket
+// npm i ajv
+
+'use strict'
+const PORT = process.env.PORT || 3000;
+// const HOST = "0.0.0.0"
+
+// Require the framework and instantiate it
+const Fastify = require('fastify')()
+
+// Declare a route
+Fastify.get('/innozee', function handler (request, reply) {
+  reply.type('application/json').code(200)
+  return { hello: 'world' }
+  //reply.send({ hello: 'world' })
+})
+
+// Run the server!
+// { port: PORT, host: HOST }
+// { port: PORT}
+Fastify.listen({ port: PORT}, (err, address) => {
+
+    if(err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at PORT: ${address}`);
+});
+
+// use URL => http://localhost:3000/
+// use URL => http://localhost:3000/innozee
